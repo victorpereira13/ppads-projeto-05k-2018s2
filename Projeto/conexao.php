d17ba7c351cb90de749b87e236ed70aa6a64d646
victorpereira13 <victor.pereira13@hotmail.com>
2018-12-14 09:59:15 -0200

    Update conexao.php

--- Projeto/conexao.php
+++ Projeto/conexao.php
@@ -1,8 +1,8 @@
 <?php
 	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "cadastro";
+	$usuario = "u205632605_proj";
+	$senha = "proj2018";
+	$dbname = "u205632605_proj";
 	
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
